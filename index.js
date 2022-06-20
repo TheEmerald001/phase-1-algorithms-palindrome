4a437af2b3e57ebcdbbc892777a694d64365f5fe
steve austin <austines811@gmail.com>
2022-06-21 01:13:38 +0300

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  function reverse(word){
+    return word.split("").reverse().join("")
+  }
+  const reverseWord = reverse(word);
+  if (word === reverseWord){
+    return true;
+  }
+  else
+    return false;
+  
+};
+
 
 /* 
   Add your pseudocode here
